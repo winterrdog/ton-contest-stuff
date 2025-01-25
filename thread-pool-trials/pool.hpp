6893ec71aa67d932cecace1c578451b7dcea3d60
winterrdog <winterrdog@protonmail.ch>
2025-01-25 10:33:21 +0300

    refactor: remove pool implementation and update test to use new thread pool class

--- thread-pool-trials/pool.hpp
+++ thread-pool-trials/pool.hpp
@@ -1,168 +1,340 @@
 #pragma once
 
 #include <atomic>
-#include <condition_variable>
+#include <concepts>
 #include <deque>
 #include <functional>
 #include <future>
-#include <iostream>
 #include <memory>
-#include <mutex>
+#include <semaphore>
 #include <thread>
-#include <utility>
-#include <vector>
+#include <type_traits>
 
-namespace my_thread_pool {
-// struct to represent a task queue for a worker thread
-struct Queue {
-  std::deque<std::function<void()>> tasks_dq; // deque to store tasks
-  std::mutex q_mutex; // mutex to control access to the task queue
-};
+#ifdef __has_include
+#if __has_include(<version>)
+#include <version>
+#endif
 
-// thread pool class with a fixed number of threads and capabale of working with
-// multiple queues and stealing tasks
-class ThreadPool {
-public:
-  explicit ThreadPool(size_t thread_cnt);
-  ~ThreadPool();
+#endif
 
-  // adds task to worker pool's task queue
-  template <class F, class... Args>
-  [[nodiscard("please make sure the output is used by producer")]] auto
-  enqueue(F &&fn, Args &&...args)
-      -> std::future<typename std::invoke_result<F, Args...>::type> {
-    /*
-    algorithm:
-        1. create a shared pointer to a packaged_task
-        2. wrap the task in a lambda
-        3. push the lambda to the task queue
-        4. notify all threads to check for new tasks
-    */
+#include "thread_safe_queue.hpp"
 
-    using ret_type = typename std::invoke_result<F, Args...>::type;
+namespace dp
+{
+namespace details
+{
 
-    auto task = std::make_shared<std::packaged_task<ret_type()>>(
-        std::bind(std::forward<F>(fn),
-                  std::forward<Args>(args)...) // bind args to function
-    );
+#ifdef __cpp_lib_move_only_function
+using default_function_type = std::move_only_function<void()>;
+#else
+using default_function_type = std::function<void()>;
+#endif
+} // namespace details
 
-    std::future<ret_type> res(task->get_future());
-    size_t worker_id = next_task_id_++ % workers_task_queues_.size();
+template <typename FunctionType = details::default_function_type, typename ThreadType = std::jthread>
+requires std::invocable<FunctionType> &&std::is_same_v<void, std::invoke_result_t<FunctionType>> class thread_pool
+{
+  public:
+    template <typename InitializationFunction = std::function<void(std::size_t)>>
+    requires std::invocable<InitializationFunction, std::size_t>
+        &&std::is_same_v<void, std::invoke_result_t<InitializationFunction, std::size_t>> explicit thread_pool(
+            const unsigned int &number_of_threads = std::thread::hardware_concurrency(),
+            InitializationFunction init = [](std::size_t) {})
+        : tasks_(number_of_threads)
     {
-      std::unique_lock<std::mutex> lock(
-          workers_task_queues_[worker_id]->q_mutex);
-      if (stop_.load()) {
-        throw std::runtime_error(
-            "cannot send in a task on a stopped thread pool.");
-      }
+        std::size_t current_id = 0;
+        for (std::size_t i = 0; i < number_of_threads; ++i)
+        {
+            priority_queue_.push_back(size_t(current_id));
+            try
+            {
+                threads_.emplace_back([&, id = current_id, init](const std::stop_token &stop_tok) {
+                    // invoke the init function on the thread
+                    try
+                    {
+                        std::invoke(init, id);
+                    }
+                    catch (...)
+                    {
+                        // suppress exceptions
+                    }
 
-      workers_task_queues_[worker_id]->tasks_dq.emplace_back(
-          [task]() { return (*task)(); });
-    }
+                    do
+                    {
+                        // wait until signaled
+                        tasks_[id].signal.acquire();
 
-    condition_.notify_all(); // notify all threads to check for new tasks
-    return res;
-  }
+                        do
+                        {
+                            // invoke the task
+                            while (auto task = tasks_[id].tasks.pop_front())
+                            {
+                                // decrement the unassigned tasks as the task is now going
+                                // to be executed
+                                unassigned_tasks_.fetch_sub(1, std::memory_order_release);
+                                // invoke the task
+                                std::invoke(std::move(task.value()));
+                                // the above task can push more work onto the pool, so we
+                                // only decrement the in flights once the task has been
+                                // executed because now it's now longer "in flight"
+                                in_flight_tasks_.fetch_sub(1, std::memory_order_release);
+                            }
 
-private:
-  std::vector<std::thread> workers_; // vector of worker threads
+                            // try to steal a task
+                            for (std::size_t j = 1; j < tasks_.size(); ++j)
+                            {
+                                const std::size_t index = (id + j) % tasks_.size();
+                                if (auto task = tasks_[index].tasks.steal())
+                                {
+                                    // steal a task
+                                    unassigned_tasks_.fetch_sub(1, std::memory_order_release);
+                                    std::invoke(std::move(task.value()));
+                                    in_flight_tasks_.fetch_sub(1, std::memory_order_release);
+                                    // stop stealing once we have invoked a stolen task
+                                    break;
+                                }
+                            }
+                            // check if there are any unassigned tasks before rotating to the
+                            // front and waiting for more work
+                        } while (unassigned_tasks_.load(std::memory_order_acquire) > 0);
 
-  // vector of task queues for each worker thread
-  std::vector<std::unique_ptr<Queue>> workers_task_queues_;
+                        priority_queue_.rotate_to_front(id);
+                        // check if all tasks are completed and release the "barrier"
+                        if (in_flight_tasks_.load(std::memory_order_acquire) == 0)
+                        {
+                            // in theory, only one thread will set this
+                            threads_complete_signal_.store(true, std::memory_order_release);
+                            threads_complete_signal_.notify_one();
+                        }
 
-  std::mutex pool_mutex_; // control safe access to task queue
+                    } while (!stop_tok.stop_requested());
+                });
+                // increment the thread id
+                ++current_id;
+            }
+            catch (...)
+            {
+                // catch all
 
-  // condition variable to notify threads of new tasks
-  std::condition_variable condition_;
-  std::atomic<bool> stop_; // flag to stop the thread pool
+                // remove one item from the tasks
+                tasks_.pop_back();
 
-  // id of the next task to be added to the pool
-  std::atomic<size_t> next_task_id_{0};
-
-  void worker_thread(size_t worker_id) {
-    for (;;) {
-      std::function<void()> task;
-
-      // get a task, if u dont hv one then steal one, otherwise wait for some to
-      // come by
-      if (try_get_task(worker_id, task) || try_steal_task(worker_id, task)) {
-        execute_task(task);
-      } else {
-        // Wait for tasks or stop signal
-        std::unique_lock<std::mutex> lock(pool_mutex_);
-        condition_.wait(lock, [this, worker_id, &task]() {
-          return stop_.load() ||
-                 !workers_task_queues_[worker_id]->tasks_dq.empty() ||
-                 try_steal_task(worker_id, task);
-        });
-
-        if (stop_.load())
-          return;
-
-        if (task) {
-          execute_task(task);
+                // remove our thread from the priority queue
+                std::ignore = priority_queue_.pop_back();
+            }
         }
-      }
-    }
-  }
-
-  bool try_get_task(size_t worker_id, std::function<void()> &task) {
-    /*
-    algorithm:
-        1. lock the task queue
-        2. check if the task queue is empty
-        3. if not empty, get the task off the dequeue belonging to the worker
-    */
-
-    auto &curr_task_queue = workers_task_queues_[worker_id];
-
-    std::lock_guard<std::mutex> lock(curr_task_queue->q_mutex);
-    if (curr_task_queue->tasks_dq.empty()) {
-      return false; // no work for thread
     }
 
-    // get the task off the dequeue
-    task = std::move(curr_task_queue->tasks_dq.front());
-    curr_task_queue->tasks_dq.pop_front();
+    ~thread_pool()
+    {
+        wait_for_tasks();
 
-    return true;
-  }
-
-  bool try_steal_task(size_t worker_id, std::function<void()> &task) {
-    /*
-    algorithm:
-        1. for each worker thread, except the current one
-        2. lock the task queue
-        3. check if the task queue is empty
-        4. if not empty, steal the task from the 'back' of the dequeue belonging
-    to the worker with work available
-    */
-
-    for (size_t i = 0; i < workers_task_queues_.size(); ++i) {
-      if (i != worker_id) {
-        auto &victim_queue = workers_task_queues_[i];
-        std::lock_guard<std::mutex> lock(victim_queue->q_mutex);
-
-        // steal the task from those with work available, provided they hv
-        if (!victim_queue->tasks_dq.empty()) {
-          task = std::move(victim_queue->tasks_dq.back());
-          victim_queue->tasks_dq.pop_back();
-
-          return true;
+        // stop all threads
+        for (std::size_t i = 0; i < threads_.size(); ++i)
+        {
+            threads_[i].request_stop();
+            tasks_[i].signal.release();
+            threads_[i].join();
         }
-      }
     }
-    return false;
-  }
 
-  void execute_task(std::function<void()> &task) {
-    try {
-      task();
-    } catch (const std::exception &e) {
-      std::cerr << "task threw exception: " << e.what() << '\n';
-    } catch (...) {
-      std::cerr << "task threw an unknown exception" << '\n';
+    /// thread pool is non-copyable
+    thread_pool(const thread_pool &) = delete;
+    thread_pool &operator=(const thread_pool &) = delete;
+
+    /**
+     * @brief Enqueue a task into the thread pool that returns a result.
+     * @details Note that task execution begins once the task is enqueued.
+     * @tparam Function An invokable type.
+     * @tparam Args Argument parameter pack
+     * @tparam ReturnType The return type of the Function
+     * @param f The callable function
+     * @param args The parameters that will be passed (copied) to the function.
+     * @return A std::future<ReturnType> that can be used to retrieve the returned
+     * value.
+     */
+    template <typename Function, typename... Args, typename ReturnType = std::invoke_result_t<Function &&, Args &&...>>
+    requires std::invocable<Function, Args...> [[nodiscard]] std::future<ReturnType> enqueue(Function f, Args... args)
+    {
+#ifdef __cpp_lib_move_only_function
+        // we can do this in C++23 because we now have support for move only
+        // functions
+        std::promise<ReturnType> promise;
+        auto future = promise.get_future();
+        auto task = [func = std::move(f), ... largs = std::move(args), promise = std::move(promise)]() mutable {
+            try
+            {
+                if constexpr (std::is_same_v<ReturnType, void>)
+                {
+                    func(largs...);
+                    promise.set_value();
+                }
+                else
+                {
+                    promise.set_value(func(largs...));
+                }
+            }
+            catch (...)
+            {
+                promise.set_exception(std::current_exception());
+            }
+        };
+        enqueue_task(std::move(task));
+        return future;
+#else
+        /*
+             * use shared promise here so that we don't break the promise later
+       (until C++23)
+             *
+             * with C++23 we can do the following:
+             *
+             * std::promise<ReturnType> promise;
+             * auto future = promise.get_future();
+             * auto task = [func = std::move(f), ...largs = std::move(args),
+                              promise = std::move(promise)]() mutable {...};
+             */
+        auto shared_promise = std::make_shared<std::promise<ReturnType>>();
+        auto task = [func = std::move(f), ... largs = std::move(args), promise = shared_promise]() {
+            try
+            {
+                if constexpr (std::is_same_v<ReturnType, void>)
+                {
+                    func(largs...);
+                    promise->set_value();
+                }
+                else
+                {
+                    promise->set_value(func(largs...));
+                }
+            }
+            catch (...)
+            {
+                promise->set_exception(std::current_exception());
+            }
+        };
+
+        // get the future before enqueuing the task
+        auto future = shared_promise->get_future();
+        // enqueue the task
+        enqueue_task(std::move(task));
+        return future;
+#endif
     }
-  }
+
+    /**
+     * @brief Enqueue a task to be executed in the thread pool. Any return value
+     * of the function will be ignored.
+     * @tparam Function An invokable type.
+     * @tparam Args Argument parameter pack for Function
+     * @param func The callable to be executed
+     * @param args Arguments that will be passed to the function.
+     */
+    template <typename Function, typename... Args>
+    requires std::invocable<Function, Args...> void enqueue_detach(Function &&func, Args &&...args)
+    {
+        enqueue_task(std::move(
+            [f = std::forward<Function>(func), ... largs = std::forward<Args>(args)]() mutable -> decltype(auto) {
+                // suppress exceptions
+                try
+                {
+                    if constexpr (std::is_same_v<void, std::invoke_result_t<Function &&, Args &&...>>)
+                    {
+                        std::invoke(f, largs...);
+                    }
+                    else
+                    {
+                        // the function returns an argument, but can be ignored
+                        std::ignore = std::invoke(f, largs...);
+                    }
+                }
+                catch (...)
+                {
+                }
+            }));
+    }
+
+    /**
+     * @brief Returns the number of threads in the pool.
+     *
+     * @return std::size_t The number of threads in the pool.
+     */
+    [[nodiscard]] auto size() const
+    {
+        return threads_.size();
+    }
+
+    /**
+     * @brief Wait for all tasks to finish.
+     * @details This function will block until all tasks have been completed.
+     */
+    void wait_for_tasks()
+    {
+        // must be a while loop to ignore spurious wake-ups
+        while (in_flight_tasks_.load(std::memory_order_acquire) > 0)
+        {
+            // wait for all tasks to finish
+            threads_complete_signal_.wait(false);
+        }
+    }
+
+    /**
+     * @brief Makes best-case attempt to clear all tasks from the thread_pool
+     * @details Note that this does not guarantee that all tasks will be cleared,
+     * as currently running tasks could add additional tasks. Also a thread could
+     * steal a task from another in the middle of this.
+     * @return number of tasks cleared
+     */
+    size_t clear_tasks()
+    {
+        size_t removed_task_count{0};
+        for (auto &task_list : tasks_)
+        {
+            removed_task_count += task_list.tasks.clear();
+        }
+        in_flight_tasks_.fetch_sub(removed_task_count, std::memory_order_release);
+        unassigned_tasks_.fetch_sub(removed_task_count, std::memory_order_release);
+
+        return removed_task_count;
+    }
+
+  private:
+    template <typename Function> void enqueue_task(Function &&f)
+    {
+        auto i_opt = priority_queue_.copy_front_and_rotate_to_back();
+        if (!i_opt.has_value())
+        {
+            // would only be a problem if there are zero threads
+            return;
+        }
+        // get the index
+        auto i = *(i_opt);
+
+        // increment the unassigned tasks and in flight tasks
+        unassigned_tasks_.fetch_add(1, std::memory_order_release);
+        const auto prev_in_flight = in_flight_tasks_.fetch_add(1, std::memory_order_release);
+
+        // reset the in flight signal if the list was previously empty
+        if (prev_in_flight == 0)
+        {
+            threads_complete_signal_.store(false, std::memory_order_release);
+        }
+
+        // assign work
+        tasks_[i].tasks.push_back(std::forward<Function>(f));
+        tasks_[i].signal.release();
+    }
+
+    struct task_item
+    {
+        dp::thread_safe_queue<FunctionType> tasks{};
+        std::binary_semaphore signal{0};
+    };
+
+    std::vector<ThreadType> threads_;
+    std::deque<task_item> tasks_;
+    dp::thread_safe_queue<std::size_t> priority_queue_;
+    // guarantee these get zero-initialized
+    std::atomic_int_fast64_t unassigned_tasks_{0}, in_flight_tasks_{0};
+    std::atomic_bool threads_complete_signal_{false};
 };
-} // namespace my_thread_pool
+} // namespace dp
--- thread-pool-trials/test-pool.cc
+++ thread-pool-trials/test-pool.cc
@@ -2,22 +2,25 @@
 
 #include "pool.hpp"
 
-using my_thread_pool::ThreadPool;
+using dp::thread_pool;
 
-TEST(ThreadPoolTest, BasicFunctionality) {
-  const unsigned limit = 200'000;
-  int num_threads = std::thread::hardware_concurrency();
+TEST(ThreadPoolTest, BasicFunctionality)
+{
+    const unsigned limit = 200'000;
+    int num_threads = std::thread::hardware_concurrency();
 
-  std::cout << "Number of threads: " << num_threads << '\n';
-  ThreadPool pool(num_threads);
-  std::vector<std::future<unsigned>> results{};
+    std::cout << "Number of threads: " << num_threads << '\n';
+    thread_pool pool(num_threads);
+    std::vector<std::future<unsigned>> results{};
 
-  for (unsigned i = 0; i < limit; i++) {
-    results.emplace_back(pool.enqueue([i] { return i * i; }));
-  }
+    for (unsigned i = 0; i < limit; i++)
+    {
+        results.emplace_back(pool.enqueue([i] { return i * i; }));
+    }
 
-  // verify output
-  for (unsigned i = 0; i < limit; i++) {
-    ASSERT_EQ(results[i].get(), i * i);
-  }
+    // verify output
+    for (unsigned i = 0; i < limit; i++)
+    {
+        ASSERT_EQ(results[i].get(), i * i);
+    }
 }
